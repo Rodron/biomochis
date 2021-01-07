0011336a0075c67b5185da5249b25ea5f8160523
AlvaroGA99 <55707621+AlvaroGA99@users.noreply.github.com>
2021-01-07 18:54:56 +0100

    scripts newBT y customMachine

---
+++ Assets/Intelligent Behaviours GUI Package/Scripts/CustomNeedMachine.cs
@@ -0,0 +1,79 @@
+
+using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+class CustomNeedMachine{
+
+    
+
+    public int currentState;
+    bool objetivoDetectado;
+    bool llegadaAlObjetivo;
+
+    
+    public bool exit;
+    
+    bool animacionTerminada;
+
+    public CustomNeedMachine(){
+        currentState = 0;
+        exit = false;
+    }
+
+    
+
+    public void Update(string typeOfObjective,bool animacionTerminada,bool objetivoDetectado, bool llegadaAlObjetivo){
+        this.animacionTerminada = animacionTerminada;
+        this.llegadaAlObjetivo = llegadaAlObjetivo;
+        this.objetivoDetectado = objetivoDetectado;
+
+        switch(currentState){
+            case 0 :
+                lookForObjectivePerception (typeOfObjective);
+                break;
+            case 1 : 
+            arrivedAtObjectivePerception();
+                break;
+            case 2 :
+                interactionWithObjectivePerception();
+                break;
+        }
+
+    }
+
+
+    
+    void lookForObjectivePerception(string typeOfObjective){
+        Debug.Log("BUSCANDO " + typeOfObjective);
+        if(objetivoDetectado ){
+            Debug.Log(typeOfObjective + "ENCONTRADO");
+            currentState = 1;
+        }
+        
+        
+    }
+
+    void arrivedAtObjectivePerception(){
+        Debug.Log("LLEGANDO A OBJETIVO" );
+        if(llegadaAlObjetivo){
+            Debug.Log("EN EL OBJETIVO");
+            currentState = 2;
+            
+        }
+    }
+
+    void interactionWithObjectivePerception(){
+        Debug.Log("INTERACTUANDO CON EL OBJETIVO");
+        if(animacionTerminada){
+            Debug.Log("HE TERMINADO DE INTERACTUAR");
+            exit = true;
+        }
+        
+    }
+
+
+
+
+
+}
---
+++ Assets/cam.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cam : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    float xRotation;
+    float yRotation;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xRotation += 10*Input.GetAxis("Mouse X");
+        yRotation += -1*10*Input.GetAxis("Mouse Y");
+        this.transform.rotation = Quaternion.Euler(yRotation,xRotation,0.0f);
+    }
+}
